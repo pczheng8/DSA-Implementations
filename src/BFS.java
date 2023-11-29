f410c207a02f964fa6da08535551a0a7d203ab42
pczheng8 <pczheng8@gmail.com>
2023-11-28 21:17:37 -0500

    Add layering and back pointers to BFS

--- src/BFS.java
+++ src/BFS.java
@@ -22,6 +22,13 @@ public class BFS {
         g.connectVertices("4", "7", 1);
         g.connectVertices("4", "8", 1);
         bfs(g.getVertex("1"));
+        Map<Vertex, Integer> layer = bfs_Layer_Pred(g.getVertex("1"));
+        for(Vertex v : layer.keySet()) {
+            System.out.println(v.label() + " " + layer.get(v));
+        }
+        for(Vertex v : pred.keySet()) {
+            System.out.println(v.label() + " " + pred.get(v));
+        }
     }
 
     public static void bfs(Vertex start) {
@@ -38,5 +45,29 @@ public class BFS {
                 }
             }
         }
+    }
+
+    public static Map<Vertex, String> pred = new LinkedHashMap<>();
+    public static Map<Vertex, Integer> bfs_Layer_Pred(Vertex start) { //what about predecessors
+        Map<Vertex, Integer> layer = new LinkedHashMap<>();
+
+        Queue<Vertex> frontier = new LinkedList<>();
+        Set<Vertex> vis = new HashSet<>();
+        frontier.add(start);
+        layer.put(start, 0);
+        pred.put(start, null);
+        while(!frontier.isEmpty()) {
+            Vertex v = frontier.remove();
+            System.out.println(v.label());
+            for(Edge e : v.outgoingEdges()) {
+                if(!vis.contains(e.destination())) {
+                    frontier.add(e.destination());
+                    vis.add(e.destination());
+                    layer.put(e.destination(), layer.get(v)+1);
+                    pred.put(e.destination(), v.label());
+                }
+            }
+        }
+        return layer;
     }
 }
